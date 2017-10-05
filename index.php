ca5a16ac4322a7abc47456ac7bb91d0f0d364767
ypk4 <yogipcoolkarni@gmail.com>
2017-10-06 00:06:08 +0530

    Depth-2 crawling done. Haven't used queue (ideal soultion), manually did 2nd level crawling.

--- index.php
+++ index.php
@@ -1,19 +1,24 @@
 <?php
-$seed_url = "https://tusharagey.github.io/Test/";
+$seed_url = "https://tusharagey.github.io/Test";
 
-function get_links($url) {
+#Empty array to hold all links to return
+    $links = array();
+
+function get_links($url, $links) {
     #Create a new DOM Document to hold our webpage structure
     $xml = new DOMDocument();
     #Load the url's contents into the DOM
     $xml->loadHTMLFile($url);
-    #Empty array to hold all links to return
-    $links = array();
-
+    
     #Loop through each <a> tag in the dom and add it to the link array
     foreach($xml->getElementsByTagName('a') as $link) {
-	if(!in_array($link->getAttribute('href'), $links)) {
-
-        	array_push($links, $link->getAttribute('href'));
+	$urlname = $link->getAttribute('href');
+	#echo $urlname;
+	if(!in_array($urlname, $links)) {
+		global $seed_url;
+		$absolute_url = $seed_url . '/' . $urlname;
+		#echo $absolute_url;
+        	array_push($links, $absolute_url);
 	        #$links[] = array('url' => $link->getAttribute('href'), 'text' => $link->nodeValue);
         	#Just ignore the above commented line
 	}
@@ -23,18 +28,26 @@ function get_links($url) {
     return $links;
 }
 
-$links = get_links($seed_url);
+$links = get_links($seed_url, $links);
 print_r($links);
 
+# Iteration - 2 (Depth - 2) crawling :-
+for($x = 0; $x < count($links); $x++) {
+	#echo $links[$x];
+	$links = get_links($links[$x], $links);
+}
+
+print_r($links);
+
+# Writing to output file :-
 $arrlength = count($links);
 
 $myfile = fopen("urls.txt", "w");
 
 for($x = 0; $x < $arrlength; $x++) {
-    echo $links[$x];
+    #echo $links[$x];
     fwrite($myfile, $links[$x]);
     fwrite($myfile, "\n");
-    echo "<br>";
 }
 
 
