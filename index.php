4f5b307e63cdd1fda88f2b1c17eb87d0ff3a5889
Tushar Agey <agey.tushar3@gmail.com>
2017-10-06 06:41:26 +0530

    closing the file and stopped unnecessary prints

--- index.php
+++ index.php
@@ -29,7 +29,7 @@ function get_links($url, $links) {
 }
 
 $links = get_links($seed_url, $links);
-print_r($links);
+#print_r($links);
 
 # Iteration - 2 (Depth - 2) crawling :-
 for($x = 0; $x < count($links); $x++) {
@@ -37,7 +37,7 @@ for($x = 0; $x < count($links); $x++) {
 	$links = get_links($links[$x], $links);
 }
 
-print_r($links);
+#print_r($links);
 
 # Writing to output file :-
 $arrlength = count($links);
@@ -49,8 +49,7 @@ for($x = 0; $x < $arrlength; $x++) {
     fwrite($myfile, $links[$x]);
     fwrite($myfile, "\n");
 }
-
-
+fclose($myfile);
 #here, we have base links
 #add these to queue and go more deep
 #collect all the possible links that we can grab
