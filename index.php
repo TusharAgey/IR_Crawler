b8230f9c18e3c441f2c4eaafbf5c1f5b01902f27
ypk4 <yogipcoolkarni@gmail.com>
2017-10-17 20:00:10 +0530

    Moved writing to output file code inside get_links() to reduce extra for loop and check for if absolute url contains coep.org.in

--- index.php
+++ index.php
@@ -1,8 +1,12 @@
 <?php
 $seed_url = "https://tusharagey.github.io/Test";
+#$seed_url = "http://www.coep.org.in";
 
-#Empty array to hold all links to return
-    $links = array();
+$links = array();	#Empty array to hold all links to return
+$crawling_depth = 100; #manual definition of crawling depth
+
+$myfile = fopen("urls.txt", "w");
+
 
 function get_links($url, $links) {
     #Create a new DOM Document to hold our webpage structure
@@ -12,46 +16,65 @@ function get_links($url, $links) {
     
     #Loop through each <a> tag in the dom and add it to the link array
     foreach($xml->getElementsByTagName('a') as $link) {
-	$urlname = $link->getAttribute('href');
-	#echo $urlname;
-	if(!in_array($urlname, $links)) {
-		global $seed_url;
-		$absolute_url = $seed_url . '/' . $urlname;
-		#echo $absolute_url;
-        	array_push($links, $absolute_url);
-	        #$links[] = array('url' => $link->getAttribute('href'), 'text' => $link->nodeValue);
-        	#Just ignore the above commented line
-	}
-    }
+    	$urlname = $link->getAttribute('href');
+    	#echo $urlname;
+    	if(!in_array($urlname, $links)) {
+    	    
+    	    global $seed_url;
+            $absolute_url = "";
+            if (strpos($urlname, 'http') !== false) { #if URL contains http or https, its absolute url. don't modify
+                $absolute_url = $urlname;
+            }
+            else{
+            	#if($urlname[strlen($string) - 1] == ','){
+            	#}
+            	#else{
+            	#}
+                $absolute_url = $seed_url . '/' . $urlname;
+            }
 
+        
+            if($absolute_url[0] != '#' and /*strpos($absolute_url, 'coep.org.in') !== false and*/ strpos($absolute_url, 'foss.coep.org.in') == false) { 
+		# if URL is anchor tag, then ignore because it eventually refers the same page.
+		# if URL is inside COEP website, then only push it to array
+                array_push($links, $absolute_url);
+                echo $absolute_url;
+		echo "\n";
+		
+		# Writing to output file :
+		global $myfile;
+		fwrite($myfile, $absolute_url);
+		fwrite($myfile, "\n");
+            }
+    	    #$links[] = array('url' => $link->getAttribute('href'), 'text' => $link->nodeValue);
+            #Just ignore the above commented line
+    	}
+    }
     //Return the links
     return $links;
 }
 
 $links = get_links($seed_url, $links);
-print_r($links);
+
+#print_r($links);
 
 # Iteration - 2 (Depth - 2) crawling :-
 for($x = 0; $x < count($links); $x++) {
 	#echo $links[$x];
 	$links = get_links($links[$x], $links);
 }
-
-print_r($links);
+#print_r($links);
 
 # Writing to output file :-
-$arrlength = count($links);
-
+/*$arrlength = count($links);
 $myfile = fopen("urls.txt", "w");
-
-for($x = 0; $x < $arrlength; $x++) {
+for($x = 0; $x < $arrlength && $x < $crawling_depth; $x++) {
     #echo $links[$x];
     fwrite($myfile, $links[$x]);
     fwrite($myfile, "\n");
-}
+} */
 
+fclose($myfile);
 
-#here, we have base links
-#add these to queue and go more deep
-#collect all the possible links that we can grab
+#here, we have base links, add these to queue and go more deep, collect all the possible links that we can grab
 ?>
