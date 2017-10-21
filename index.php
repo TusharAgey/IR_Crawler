ac95d037a09880c17622d48d951fec5ca07ca5a9
Tushar Agey <agey.tushar3@gmail.com>
2017-10-21 06:31:12 +0530

    improved elasticsearch connection. addressed the issue of JSON string validation. Indexing is correct now so as the retrieval

--- index.php
+++ index.php
@@ -8,9 +8,11 @@ $crawling_depth = 100; #manual definition of crawling depth
 
 $myfile = fopen("urls.txt", "w");
 
-$count = 0;
+$count = 1;
 
-
+function fixString($str){ #removing the special characters. Special characters are not allowed in JSON such as (") in a string.
+    return preg_replace('/[^a-zA-Z0-9_ %\[\]\.\(\)%&-]/s', ' ', $str);
+}
 function get_links($url, & $links) {					# '&' indicates pass by reference
     #Create a new DOM Document to hold our webpage structure
     $xml = new DOMDocument();
@@ -20,28 +22,29 @@ function get_links($url, & $links) {					# '&' indicates pass by reference
     # Get title, content of page and feed them to indexer :-
     $title_list = $xml->getElementsByTagName('title');
     foreach ($title_list as $title) {
-    	$title_str = $title->nodeValue; #, PHP_EOL;
+    	$title_str = fixString(trim($title->nodeValue)); #, PHP_EOL; trim for removing any whitespaces/unknown characters around the string
     }
     
     $body_tags = $xml->getElementsByTagName('body');
     foreach ($body_tags as $body) {
-    	$body_content = $body->nodeValue; #, PHP_EOL;
+    	$body_content = fixString(trim($body->nodeValue)); #, PHP_EOL;
     }
     
     # Elastic search code :-
     $ch = curl_init();
     global $count;
-    $url = "http://localhost:9200/url-test/document/". $count. "?pretty=true";
+    $Curlurl = "http://localhost:9200/url-test/document/". $count. "?pretty=true"; #connection to elasticsearch
     $count++;
     #$param = "{ \"title\" : \"myWebsite\", \"link\" : \"https://tusharagey.github.io/Test\", \"content\" : \"Small with URL https://tusharagey.github.io/Test.\" }";
-    $param = "{ \"title\" : $title_str, \"link\" : $url, \"content\" : $body_content }";
-    curl_setopt($ch, CURLOPT_URL ,$url);
+    $param = '{ "title" : "'.$title_str.'", "link" : "'.$url.'", "content" : "'.$body_content.'" }';
+    
+    curl_setopt($ch, CURLOPT_URL ,$Curlurl); #curl command set to send query to elasticsearch
     curl_setopt($ch, CURLOPT_HEADER, 0);
     curl_setopt($ch, CURLOPT_POSTFIELDS, $param);
     $result = curl_exec($ch);
     echo $result;
+    echo "<br/>";
     curl_close($ch);
-
     
     #Loop through each <a> tag in the dom and add it to the link array
     foreach($xml->getElementsByTagName('a') as $link) {
@@ -49,34 +52,26 @@ function get_links($url, & $links) {					# '&' indicates pass by reference
     	#echo $urlname;
     	if(!in_array($urlname, $links) && $urlname[0] != '#' && strcmp($urlname, "/") != 0) {
     		global $seed_url;
-            	$absolute_url = "";
-            	
-            	if (strpos($urlname, 'http') !== false) { #if URL contains http or https, it is absolute url; don't modify
-                	$absolute_url = $urlname;
-            	}
-            	else{
-            		if($urlname[strlen($urlname) - 1] == '/'){		# If the relative url ends with a '/', remove it
+        	$absolute_url = "";    	
+            if (strpos($urlname, 'http') !== false) { #if URL contains http or https, it is absolute url; don't modify
+            	$absolute_url = $urlname;
+        	}
+        	else{
+            	if($urlname[strlen($urlname) - 1] == '/'){		# If the relative url ends with a '/', remove it
 				$urlname = rtrim($urlname, '/');					
 				#$absolute_url = $seed_url . '/' . rtrim($urlname, '/');
 				# This probably caused some double '/'s in url which we saw that day in lab, so this might solve it
-            		}
-            		
-            		if($urlname[0] == '/') {				# If relative url starts with a '/', remove it
-            			$urlname = ltrim($urlname, '/');
-            		}
-			
-            		$absolute_url = $seed_url . '/' . $urlname;
-            	}
-		
+        	}
+        	if($urlname[0] == '/') {				# If relative url starts with a '/', remove it
+    			$urlname = ltrim($urlname, '/');
+      		}			
+    		$absolute_url = $seed_url . '/' . $urlname;
+    	}		
 		if( ($absolute_url[0] != '#') && (strpos($absolute_url, 'javascript') == false) && (strpos($absolute_url, 'coep.org.in') !== false) && (strpos($absolute_url, 'foss.coep.org.in') == false) && (!in_array($absolute_url, $links)) && (strcmp($absolute_url, "http://www.coep.org.in/") != 0) )
 		{
 			# if URL is anchor tag, then ignore because it eventually refers the same page.
 			# if URL is inside COEP website, then only push it to array
-		        array_push($links, $absolute_url);
-		        
-			echo $absolute_url;
-			echo "\n";
-		
+            array_push($links, $absolute_url);
 			# Writing to output file :
 			global $myfile;
 			fwrite($myfile, $absolute_url);
@@ -87,11 +82,9 @@ function get_links($url, & $links) {					# '&' indicates pass by reference
     //Return the links
     #return $links;		# Modifying the actual argument array itself, so no need of returning (pass by reference)
 }
-
-
 $links = array();
 get_links($seed_url, $links);
-print_r($links);
+#print_r($links);
 
 
 # Iteration - 2 (Depth - 2) crawling :-
@@ -101,16 +94,4 @@ for($x = 0; $x < count($links) && $x < $crawling_depth; $x++) {
 }
 #print_r($links);
 fclose($myfile);
-
-
-#testing elasticsearch API:
-/*$ch = curl_init();
-$url = "http://localhost:9200/url-test/document/1?pretty=true";
-$param = "{ \"title\" : \"myWebsite\", \"link\" : \"https://tusharagey.github.io/Test\", \"content\" : \"Small with URL https://tusharagey.github.io/Test.\" }";
-curl_setopt($ch, CURLOPT_URL ,$url);
-curl_setopt($ch, CURLOPT_HEADER, 0);
-curl_setopt($ch, CURLOPT_POSTFIELDS, $param);
-$result = curl_exec($ch);
-echo $result;
-curl_close($ch);*/
 ?>
