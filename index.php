85fa9c0c87964bbc5f29e7845c5b4407ddfaca8e
akshadatonde997 <akshadatonde997@gmail.com>
2017-11-10 16:13:26 +0530

    handled all types of urls

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
-#$seed_url = "https://tusharagey.github.io/Test";
-$seed_url = "http://www.coep.org.in";
+$seed_url = "https://tusharagey.github.io/Test/";
+#$seed_url = "http://www.coep.org.in/";
 
 #$links = array();	#Empty array to hold all links to return
 
@@ -18,7 +18,7 @@ function get_links($url, & $links) {					# '&' indicates pass by reference
     $xml = new DOMDocument();
     #Load the url's contents into the DOM
     $xml->loadHTMLFile($url);
-    
+    /* 
     # Get title, content of page and feed them to indexer :-
     $title_list = $xml->getElementsByTagName('title');
     foreach ($title_list as $title) {
@@ -30,7 +30,7 @@ function get_links($url, & $links) {					# '&' indicates pass by reference
 
     
     # Elastic search code :-
-    $ch = curl_init();
+   $ch = curl_init();
     global $count;
     $Curlurl = "http://localhost:9200/url-test/document/". $count. "?pretty=true"; #connection to elasticsearch
     $count++;
@@ -43,30 +43,52 @@ function get_links($url, & $links) {					# '&' indicates pass by reference
     $result = curl_exec($ch);
     echo $result;
     echo "<br/>";
-    curl_close($ch);
+    curl_close($ch);*/
     
     #Loop through each <a> tag in the dom and add it to the link array
     foreach($xml->getElementsByTagName('a') as $link) {
     	$urlname = $link->getAttribute('href');
     	#echo $urlname;
     	if(!in_array($urlname, $links) && $urlname[0] != '#' && strcmp($urlname, "/") != 0) {
+    	
     		global $seed_url;
-        	$absolute_url = "";    	
-            if (strpos($urlname, 'http') !== false) { #if URL contains http or https, it is absolute url; don't modify
+        	$absolute_url = "";
+            	
+            if ((strpos($urlname, 'http') !== false) ) { #if URL contains http or https, it is absolute url; don't modify
             	$absolute_url = $urlname;
         	}
         	else{
             	if($urlname[strlen($urlname) - 1] == '/'){		# If the relative url ends with a '/', remove it
-				$urlname = rtrim($urlname, '/');					
-				#$absolute_url = $seed_url . '/' . rtrim($urlname, '/');
-				# This probably caused some double '/'s in url which we saw that day in lab, so this might solve it
-        	}
-        	if($urlname[0] == '/') {				# If relative url starts with a '/', remove it
-    			$urlname = ltrim($urlname, '/');
-      		}			
-    		$absolute_url = $seed_url . '/' . $urlname;
-    	}		
-		if( ($absolute_url[0] != '#') && (strpos($absolute_url, 'javascript') == false) && (strpos($absolute_url, 'coep.org.in') !== false) && (strpos($absolute_url, 'foss.coep.org.in') == false) && (!in_array($absolute_url, $links)) && (strcmp($absolute_url, "http://www.coep.org.in/") != 0) )
+					$urlname = rtrim($urlname, '/');					
+					#$absolute_url = $seed_url . '/' . rtrim($urlname, '/');
+					# This probably caused some double '/'s in url which we saw that day in lab, so this might solve it
+					$absolute_url = $seed_url . '/' . $urlname;
+        		}
+        		else if((strcmp($urlname[0],"/")==0)&&(strcmp($urlname[1],"/")==0)){ # If relative URL starts with double // then append http to relative URL
+    				$absolute_url='http:'.$urlname;
+    			}
+        		else if($urlname[0] == '/') {				# If relative url starts with a '/', remove it
+    				$urlname = ltrim($urlname, '/');
+    				$absolute_url = $seed_url . '/' . $urlname;
+      			}
+      			else if((strcmp($urlname[0],".")==0)&&(strcmp($urlname[0],"/")==0)){	# If Relative URL starts with '.' and'/'
+      				$absolute_url=$url.'/'.$urlname;		
+    			}
+            	else if ((strpos($urlname, 'http') !== false) && ((strcmp($urlname[23],"."))==0)) { #if URL contains http or https, it is absolute url; don't modify
+            	#$absolute_url = $urlname;
+            		$urlnew = explode('/', $url);
+					array_pop($urlnew);
+					$urlnew = implode('/', $urlnew);
+					$absolute_url = $urlnew . '/' . $urlname;
+        		}
+        		else{
+        		 $absolute_url = $seed_url . '/' . $urlname;# IF nothing above condiontion matches the come here 
+        		}
+        		
+    			#$absolute_url = $seed_url . '/' . $urlname;
+    		}
+    		echo $absolute_url;
+		if( ($absolute_url[0] != '#') && (strpos($absolute_url, 'javascript') == false))// && (strpos($absolute_url, 'coep.org.in') !== false) && (strpos($absolute_url, 'foss.coep.org.in') == false) && (!in_array($absolute_url, $links)) && (strcmp($absolute_url, "http://www.coep.org.in/") != 0) )
 		{
 			# if URL is anchor tag, then ignore because it eventually refers the same page.
 			# if URL is inside COEP website, then only push it to array
